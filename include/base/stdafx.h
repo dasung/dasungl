9ac9fba510fe543015919bc35bb7d14ef71927e0
dasun <hmdtharaka@gmail.com>
2019-11-21 20:13:33 +0530

    Added base_lib

---
+++ include/base/stdafx.h
@@ -0,0 +1,23 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+//
+
+#pragma once
+
+#include <algorithm>
+#include <cassert>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <sys/stat.h>
+#include <vector>
+#include <Windows.h>
+
+#if !defined GENERATE_ERROR_LIST
+#include <codecvt>
+#include <tchar.h>
+#endif
